25f202eecbfd9f8ff52abb7f041217c1cb71da56
rasouljabbari <rasoul_jabbari_1377@yahoo.com>
2021-12-05 15:49:31 +0330

    Dropzone

--- src/Components/Dropzone/Dropzone.js
+++ src/Components/Dropzone/Dropzone.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react'
 // import Button from '@material-ui/core/Button';
 
 import {DropzoneArea} from 'material-ui-dropzone'
+import Spinner from "../Spinner/Spinner";
 
 class Dropzone extends Component {
     constructor(props) {
@@ -26,6 +27,7 @@ class Dropzone extends Component {
     }
 
     handleSave(files) {
+        console.log('gio')
         //Saving files to state for further use and closing Modal.
         this.setState({
             files: files,
@@ -33,9 +35,15 @@ class Dropzone extends Component {
         });
     }
 
+    handleShowSpinner(file){
+        console.log(file , 'gio')
+    }
+
     render() {
         return (
-            <div>
+            this.state.open ?
+                <Spinner message='آپلود عکس'/> :
+                <div>
                 {/*Upload in modal*/}
                 {/*<Button onClick={this.handleOpen.bind(this)}>*/}
                 {/*    Add Image*/}
@@ -56,7 +64,9 @@ class Dropzone extends Component {
                 <DropzoneArea
                     dropzoneText={"Drag and drop an image here or click"}
                     open={this.state.open}
+                    // onDrop={this.handleOpen.bind(this)}
                     filesLimit={1000}
+                    onAdd={this.handleShowSpinner.bind(this)}
                     onSave={this.handleSave.bind(this)}
                     acceptedFiles={['image/jpeg', 'image/png', 'image/bmp']}
                     maxFileSize={5000000}
